fec2ea39b3420ec11345159bc547a8dc5980bb7d
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-09-16 19:52:13 +0200

    correccion

--- 1º TRIMESTRE/PRACTICAS/Python/06_tablaMultiplicar/tablaMultiplicar.py
+++ 1º TRIMESTRE/PRACTICAS/Python/06_tablaMultiplicar/tablaMultiplicar.py
@@ -1,5 +1,5 @@
-numeroInicio = float(input("Dime de qué número quieres saber la tabla de multiplicar: "))
+numero = float(input("Dime de qué número quieres saber la tabla de multiplicar: "))
 numeroFinal = int(input("Dime hasta que número quieres que te haga la tabla de multiplicar: "))
 
 for i in range(0, numeroFinal):
-    print(numeroInicio, " x ", i, " = ", numeroInicio*i)
+    print(numero, " x ", i, " = ", numero*i)
